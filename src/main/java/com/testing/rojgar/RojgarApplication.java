0c993f1bd25f5a567f65819b59b98a86d46fc464
santoshpaudel143 <santoshpaudel097@gmail.com>
2021-07-14 18:05:07 +0545

    Adding project to git

---
+++ src/main/java/com/testing/rojgar/RojgarApplication.java
@@ -0,0 +1,13 @@
+package com.testing.rojgar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RojgarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RojgarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testing/rojgar/RojgarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testing.rojgar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RojgarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
