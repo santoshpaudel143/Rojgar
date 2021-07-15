0f78970c53f04b0f0ff01087c05c3312201b402b
santoshpaudel143 <santoshpaudel097@gmail.com>
2021-07-15 18:45:23 +0545

    table column constraints file added

---
+++ src/main/java/com/testing/rojgar/utill/TableColumnNameConstants.java
@@ -0,0 +1,43 @@
+package com.testing.rojgar.utill;
+
+/**
+ ** Author: Santosh Paudel
+ **/
+
+public interface TableColumnNameConstants {
+
+	/*
+	 * ####################################################################
+	 * CONSTANTS FOR OTHER VARIABLE
+	 * ####################################################################
+	 */
+
+	String MALE = "Male";
+	String FEMALE = "Female";
+
+	/*
+	 * ####################################################################
+	 * CONSTANTS FOR COMMON FIELDS
+	 * ####################################################################
+	 */
+	String ID = "id";
+	String STATUS = "status";
+	String CREATED_IN = "created_in";
+
+	/*
+	 * ####################################################################
+	 * CONSTANTS FOR USER REGISTRATION
+	 * ####################################################################
+	 */
+
+	String USER_REGISTRATION = "user_registration";
+	String FULL_NAME = "full_name";
+	String USER_NAME = "user_name";
+	String ADDRESS = "address";
+	String CONTACT = "contact";
+	String EMAIL = "email";
+	String DATE_OF_BIRTH = "date_of_birth";
+	String GENDER = "gender";
+	String PASSWORD = "password";
+
+}
