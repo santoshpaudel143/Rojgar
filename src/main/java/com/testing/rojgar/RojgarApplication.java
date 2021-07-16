c923c7fe3c891482319a8587eecb13c49fbe0083
santoshpaudel143 <santoshpaudel097@gmail.com>
2021-07-16 10:44:16 +0545

    application.properties file updated

--- src/main/java/com/testing/rojgar/RojgarApplication.java
+++ src/main/java/com/testing/rojgar/RojgarApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableAuthorizationServer
+//@EnableResourceServer
 public class RojgarApplication {
 	private static final Logger logger = LoggerFactory.getLogger(RojgarApplication.class);
 
